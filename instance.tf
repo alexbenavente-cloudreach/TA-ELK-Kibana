dc819c6144de78b9a35f4873b7aa5b2dab8db1e2
aurion-cloud <aurion.howard@cloudreach.com>
2022-04-26 10:05:21 -0400

    kibana server provider updates

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Kibana" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = data.aws_ami.aws_basic_ami.id
   instance_type          = "t3.medium"
-  subnet_id              = data.aws_subnet.kibana.id
+  subnet_id              = data.aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.kibana_sg.id]
   key_name               = var.my_keypair
   count                  = var.number_of_instances
---
+++ main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "aws_basic_ami" {
+    owners = [var.aws_owner_id]
+    most_recent = true
+    filter {
+        name = "name"
+        values = [var.aws_ami_name]
+    }
+}
+
+
+data "aws_vpc" "main_vpc"{
+    filter{
+        name = "tag:Name"
+        values = [var.vpc_name]
+    }
+}
+
+data "aws_subnet" "public"{
+    filter{
+        name = "tag:Name"
+        values = [var.kibana_subnet_name]
+    }
+}
+
