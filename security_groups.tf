06287e475be8b94ae356d0531365453e59518caf
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-04-27 14:28:23 -0400

    ran a terraform fmt

--- security_groups.tf
+++ security_groups.tf
@@ -2,28 +2,28 @@ resource "aws_security_group" "kibana_sg" {
   vpc_id = data.aws_vpc.main_vpc.id
   ingress {
     description = "ingress rules"
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
   }
   ingress {
     description = "Allow ssh into bastion host"
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 5601
-    protocol = "tcp"
-    to_port = 5601
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 5601
+    protocol    = "tcp"
+    to_port     = 5601
   }
 
   egress {
     description = "egress rules"
-    cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
   }
-  tags={
-    Name="kibana_sg"
+  tags = {
+    Name = "kibana_sg"
   }
 }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,6 +1,6 @@
 aws_owner_id        = "099720109477"
 aws_ami_name        = "*ubuntu-focal-20.04-amd64-server-*"
 vpc_name            = "elk1-vpc"
-kibana_subnet_name     = "elk1-public-a"
+kibana_subnet_name  = "elk1-public-a"
 my_keypair          = "elk_keypair"
 number_of_instances = 1
