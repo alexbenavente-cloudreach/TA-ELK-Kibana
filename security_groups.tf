042a492316a03e865f351ce1f2845a2aec0d8f21
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-05-03 17:43:24 -0400

    adding github actions

--- security_groups.tf
+++ security_groups.tf
@@ -1,14 +1,14 @@
 resource "aws_security_group" "kibana_sg" {
   vpc_id = data.aws_vpc.main_vpc.id
   ingress {
-    description = "ingress rules"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "ssh"
+    cidr_blocks = ["192.168.0.0/16"]
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
   }
   ingress {
-    description = "Allow ssh into bastion host"
+    description = "Allow kibana traffic"
     cidr_blocks = ["0.0.0.0/0"]
     from_port   = 5601
     protocol    = "tcp"
